fe0881b11d6b432fb19507e3329defca461606b4
Kaydhee <98499125+Kaydhee@users.noreply.github.com>
2022-10-14 14:24:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Revealing Section on Scroll
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section__hidden");
+  // unobserving
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section__hidden");
+});
