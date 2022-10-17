35bf222ae2e61024116e980d005f05bcb74ae7b6
Kaydhee <98499125+Kaydhee@users.noreply.github.com>
2022-10-17 05:35:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,26 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section__hidden");
 });
+
+// Getting input and sending
+const entryForm = document.querySelectorAll(".hero__form");
+
+// console.log(entryForm);
+function sendMail(mail) {
+  const data = { email: mail };
+
+  fetch("https://courageous-yak-veil.cyclic.app/waitlist/add", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.JSON())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
